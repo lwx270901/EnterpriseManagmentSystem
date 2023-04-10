68d7e0a30e3009e49481b223d5ad236cc65413aa
La Duong Xuan Linh <67784263+link5401@users.noreply.github.com>
2023-04-10 15:30:57 +0700

    header footer role division

--- index.php
+++ index.php
@@ -3,37 +3,45 @@
 // Start the session
 session_start();
 
+$_SESSION['user'] = 'linh';
+$_SESSION['role'] = 'director';
+// $_SESSION['role'] = 'department_head';
+// $_SESSION['role'] = 'employee';
+
 // Include the configuration file
 include_once 'includes/config.php';
 
 // Include the functions file
 include_once 'includes/functions.php';
 
+
 // Check if the user is logged in
-if (!is_logged_in()) {
-  header('Location: login.php');
-  exit;
-}
+// // $_SESSION['USER']
+// if (isset($_SESSION['user'])) {
+//   header('Location: login.php');
+//   exit;
+// }
 
 // Get the user's role
-$user_role = get_user_role();
+$user_role = $_SESSION['role'];
 
 // Include the header template
 include_once 'templates/header.php';
 
 // Display the appropriate page based on the user's role
-switch ($user_role) {
-  case 'director':
-    include_once 'pages/dashboard.php';
-    break;
-  case 'department_head':
-    include_once 'pages/task_assign.php';
-    break;
-  case 'employee':
-    include_once 'pages/task_submit.php';
-    break;
-}
+// switch ($user_role) {
+//   case 'director':
+//     include_once 'pages/dashboard.php';
+//     break;
+//   case 'department_head':
+//     include_once 'pages/task_assign.php';
+//     break;
+//   case 'employee':
+//     include_once 'pages/task_submit.php';
+//     break;
+// }
 
 // Include the footer template
+
 include_once 'templates/footer.php';
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,19 @@
 <!-- header template for each page -->
+<!-- <section>
+            <ul class="nav-bar row px-0 g-0">
+                <li class="col-3 px-0"><a href="?page=home">Home</a></li>
+                <li class="col-3 px-0"><a href="?page=products">Products</a></li>
+                <li class="col-3 px-0"><a href="?page=register">Register</a></li>
+                <li class="col-3 px-0"><a href="?page=login">Login</a></li>
+            </ul>
+
+        </section> -->
+<?php if ($_SESSION['role'] == 'director') : ?>
+
+
+<?php elseif ($_SESSION['role'] == 'department_head') : ?>
+
+<?php elseif ($_SESSION['role'] == 'department_head') : ?>
+
+
+<? endif; ?>
