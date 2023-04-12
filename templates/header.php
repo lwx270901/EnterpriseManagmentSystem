8c1e725a4918af0fc9cf24c6abce729d44870256
La Duong Xuan Linh <67784263+link5401@users.noreply.github.com>
2023-04-12 16:55:11 +0700

    header modifications

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <!-- header template for each page -->
-<!-- 
+<!-- <section>
             <ul class="nav-bar row px-0 g-0">
                 <li class="col-3 px-0"><a href="?page=home">Home</a></li>
                 <li class="col-3 px-0"><a href="?page=products">Products</a></li>
@@ -7,46 +7,66 @@
                 <li class="col-3 px-0"><a href="?page=login">Login</a></li>
             </ul>
 
-         -->
-<?php if ($_SESSION['role'] == 'director'): ?>
+        </section> -->
+<?php if ($_SESSION['role'] == 'director') : ?>
 
+    <div class="header">
+        <div class="header-content">
+            <div class="user-info"></div>
+            <div class="nav-bar-container">
+                <ul class="nav-bar row px-0     ">
+                    <li class="col-6 px-0"><a href="?func=deparment-dashboard">Dashboard</a></li>
+                    <li class="col-6 px-0"><a href="?func=logout">Logout</a></li>
+            </div>
+            </ul>
+        </div>
+    </div>
 
-    <ul class="nav-bar row px-0     ">
-        <li class="col-4 px-0"><a href="?page=dashboard">Dashboard</a></li>
-        <li class="col-4 px-0"><a href="?page=assignDH">Assign deparment heads</a></li>
-        <li class="col-4 px-0"><a href="?page=logout">Logout</a></li>
+<?php elseif ($_SESSION['role'] == 'department_head') : ?>
 
-    </ul>
+    <div class="header">
+        <div class="header-content">
+            <div class="user-info"></div>
+            <div class="nav-bar-container">
+                <ul class="nav-bar row px-0 g-0">
+                    <li class="col-3 px-0"><a href="?func=emp-dashboard">Dashboard</a></li>
+                    <li class="col-3 px-0"><a href="?func=assignEmployee">Assign Employee</a></li>
+                    <li class="col-3 px-0"><a href="?func=assignTask">Assign Task</a></li>
+                    <li class="col-3 px-0"><a href="?func=logout">Logout</a></li>
+                </ul>
+            </div>
+        </div>
 
+    </div>
 
+<?php elseif ($_SESSION['role'] == 'employee') : ?>
+    <div class="header">
+        <div class="header-content">
+            <div class="user-info"></div>
+            <div class="nav-bar-container">
+                <ul class="nav-bar row px-0 g-0">
+                    <li class="col-4 px-0"><a href="?func=viewTask">viewTask</a></li>
+                    <li class="col-4 px-0"><a href="?func=submitTask">submit Task</a></li>
+                    <li class="col-4 px-0"><a href="?func=logout">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-<?php elseif ($_SESSION['role'] == 'department_head'): ?>
-
-
-    <ul class="nav-bar row px-0">
-        <li class="col-3 px-0"><a href="?page=dashboard">Dashboard</a></li>
-        <li class="col-3 px-0"><a href="?page=assignEmployee">Assign Employee</a></li>
-        <li class="col-3 px-0"><a href="?page=assignTask">Assign Task</a></li>
-        <li class="col-3 px-0"><a href="?page=logout">Logout</a></li>
-    </ul>
-
-
-<?php elseif ($_SESSION['role'] == 'employee'): ?>
-
-    <ul class="nav-bar row px-0">
-        <li class="col-4 px-0"><a href="?page=viewTask">viewTask</a></li>
-        <li class="col-4 px-0"><a href="?page=submitTask">submit Task</a></li>
-        <li class="col-4 px-0"><a href="?page=logout">Logout</a></li>
-    </ul>
-<?php else: ?>
-
-    <ul class="nav-bar row px-0">
-        <li class="col px-0"><a href="?page=home">Home</a></li>
-
-        <li class="col px-0"><a href="?page=login">Login</a></li>
-    </ul>
+<?php else : ?>
+    <div class="header">
+        <div class="header-content">
+            <div class="user-info"></div>
+            <div class="nav-bar-container">
+                <ul class="nav-bar row px-0 g-0">
+                    <li class="col-12 px-0"><a href="?func=login">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 <?php endif; ?>
-<?php
+
+<?php 
 $page = isset($_GET["page"]) ? $_GET["page"] : 0;
 switch ($page) {
     case "logout":
