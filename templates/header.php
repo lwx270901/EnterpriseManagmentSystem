46a454fb5037ea93d45dc1e817da32a90b040234
La Duong Xuan Linh <67784263+link5401@users.noreply.github.com>
2023-04-12 17:58:58 +0700

    init

--- templates/header.php
+++ templates/header.php
@@ -11,63 +11,97 @@
 <?php if ($_SESSION['role'] == 'director') : ?>
 
     <div class="header">
-        <div class="header-content">
-            <div class="user-info"></div>
-            <div class="nav-bar-container">
-                <ul class="nav-bar row px-0     ">
-                    <li class="col-6 px-0"><a href="?func=deparment-dashboard">Dashboard</a></li>
-                    <li class="col-6 px-0"><a href="?func=logout">Logout</a></li>
+        <div class="row g-0">
+            <div class="col-lg-2 col-sm-0">
             </div>
-            </ul>
+            <div class="col-lg nav-bar-container">
+                <ul class="nav-bar row g-0 px-0  ">
+                    <li class="col-6 px-0"><a href="?func=director-dashboard">Dashboard</a></li>
+                    <li class="col-6 px-0"><a href="?func=logout">Logout</a></li>
+
+                </ul>
+            </div>
+            <div class="user col-lg-2 col-sm-0">
+                <div class="row gx-0 px-0">
+                    <div class="col-2 px-0">
+                        <img width="60px" height="60px" src="./assets//images/user.png" alt="">
+                    </div>
+                    <span class="user-text col px-0">
+                        <span id="user-name">Username</span>
+                        <br>
+                        <span id="user-role">User role</span>
+                        <br>
+                        <span id="user-level">User level</span>
+                    </span>
+                </div>
+            </div>
+
         </div>
     </div>
 
 <?php elseif ($_SESSION['role'] == 'department_head') : ?>
-
     <div class="header">
-        <div class="header-content">
-            <div class="user-info"></div>
-            <div class="nav-bar-container">
-                <ul class="nav-bar row px-0 g-0">
-                    <li class="col-3 px-0"><a href="?func=emp-dashboard">Dashboard</a></li>
-                    <li class="col-3 px-0"><a href="?func=assignEmployee">Assign Employee</a></li>
-                    <li class="col-3 px-0"><a href="?func=assignTask">Assign Task</a></li>
-                    <li class="col-3 px-0"><a href="?func=logout">Logout</a></li>
+        <div class="row g-0">
+            <div class="col-lg-2 col-sm-0">
+            </div>
+            <div class="col-lg nav-bar-container">
+                <ul class="nav-bar row g-0 px-0  ">
+                    <li class="col-6 px-0"><a href="?func=deparment-dashboard">Dashboard</a></li>
+                    <li class="col-6 px-0"><a href="?func=logout">Logout</a></li>
+
                 </ul>
             </div>
-        </div>
+            <div class="user col-lg-2 col-sm-0">
+                <div class="row gx-0 px-0">
+                    <div class="col-2 px-0">
+                        <img width="60px" height="60px" src="./assets//images/user.png" alt="">
+                    </div>
+                    <span class="user-text col px-0">
+                        <span id="user-name">Username</span>
+                        <br>
+                        <span id="user-role">User role</span>
+                        <br>
+                        <span id="user-level">User level</span>
+                    </span>
+                </div>
+            </div>
 
+        </div>
     </div>
 
 <?php elseif ($_SESSION['role'] == 'employee') : ?>
     <div class="header">
-        <div class="header-content">
-            <div class="user-info"></div>
-            <div class="nav-bar-container">
-                <ul class="nav-bar row px-0 g-0">
-                    <li class="col-4 px-0"><a href="?func=viewTask">viewTask</a></li>
-                    <li class="col-4 px-0"><a href="?func=submitTask">submit Task</a></li>
-                    <li class="col-4 px-0"><a href="?func=logout">Logout</a></li>
-                </ul>
+        <div class="row g-0">
+            <div class="col-lg-2 col-sm-0">
             </div>
-        </div>
-    </div>
+            <div class="col-lg nav-bar-container">
+                <ul class="nav-bar row g-0 px-0  ">
+                    <li class="col-6 px-0"><a href="?func=employee-dashboard">Dashboard</a></li>
+                    <li class="col-6 px-0"><a href="?func=logout">Logout</a></li>
 
-<?php else : ?>
-    <div class="header">
-        <div class="header-content">
-            <div class="user-info"></div>
-            <div class="nav-bar-container">
-                <ul class="nav-bar row px-0 g-0">
-                    <li class="col-12 px-0"><a href="?func=login">Login</a></li>
                 </ul>
             </div>
+            <div class="user col-lg-2 col-sm-0">
+                <div class="row gx-0 px-0">
+                    <div class="col-2 px-0">
+                        <img width="60px" height="60px" src="./assets//images/user.png" alt="">
+                    </div>
+                    <span class="user-text col px-0">
+                        <span id="user-name">Username</span>
+                        <br>
+                        <span id="user-role">User role</span>
+                        <br>
+                        <span id="user-level">User level</span>
+                    </span>
+                </div>
+            </div>
+
         </div>
     </div>
 <?php endif; ?>
-
-<?php 
-$page = isset($_GET["page"]) ? $_GET["page"] : 0;
+<hr>
+<?php
+$page = isset($_GET["func"]) ? $_GET["func"] : 0;
 switch ($page) {
     case "logout":
         logout();
