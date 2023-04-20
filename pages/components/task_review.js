490684d2ef73896e9639192ef46a158cc6f0f326
LWX270901 <letungocminh@gmail.com>
2023-04-20 22:37:57 +0700

    xen thu

--- pages/components/task_review.js
+++ pages/components/task_review.js
@@ -1,51 +1,55 @@
-$(document).ready(function() {
-    const taskSearch = $('#task-search');
-    const taskList = $('#task-list');
-    
-    taskSearch.keyup(function() {
-      const searchTerm = $(this).val();
-      
-      if (searchTerm.length >= 3) {
-        $.ajax({
-          url: 'search-tasks.php',
-          type: 'GET',
-          data: { q: searchTerm },
-          success: function(response) {
-            taskList.empty();
-            
-            $.each(response, function(index, task) {
-              const taskItem = $('<li>').text(task.name);
-              
-              taskItem.click(function() {
-                taskSearch.val(task.name);
-                taskList.empty();
-              });
-              
-              taskList.append(taskItem);
-            });
-          }
-        });
-      } else {
-        taskList.empty();
+// Retrieve tasks for the logged-in user using Ajax
+function getTasks() {
+  // Get the task list table body
+  const taskList = document.getElementById("task-list");
+
+  // Create an Ajax request
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Parse the response as JSON
+      const tasks = JSON.parse(this.responseText);
+
+      // Remove all existing task rows
+      taskList.innerHTML = "";
+
+      // Populate the task rows from the template
+      const taskTemplate = document.getElementById("task-template");
+      for (let task of tasks) {
+        // Clone the task template and populate the task data
+        const taskRow = taskTemplate.content.cloneNode(true);
+        taskRow.querySelector(".task-name").textContent = task.Description;
+        taskRow.querySelector(".task-status").textContent = task.sStatus;
+        taskRow.querySelector("form").dataset.taskId = task.TaskId;
+
+        // Append the task row to the task list
+        taskList.appendChild(taskRow);
       }
-    });
-    
-    $('.review-task-form').submit(function(event) {
-      event.preventDefault();
-      
-      const formData = $(this).serialize();
-      
-      $.ajax({
-        url: '/review-task.php',
-        type: 'POST',
-        data: formData,
-        success: function(response) {
-          alert('Form submitted successfully!');
-        },
-        error: function(xhr) {
-          alert('Error submitting form!');
-        }
-      });
-    });
-  });
-  
+    }
+  };
+  xhr.open("GET", "get_tasks.php");
+  xhr.send();
+}
+
+// Update a task's status and deadline using Ajax
+function updateTask(button) {
+  const form = button.closest(".task-form");
+  const taskId = form.dataset.TaskId;
+  const formData = new FormData(form);
+
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Parse the response as JSON
+      const response = JSON.parse(this.responseText);
+
+      // Update the task status and deadline on the page
+      const statusCell = form.parentNode.querySelector(".Status");
+      const deadlineCell = statusCell.nextElementSibling;
+      statusCell.textContent = response.status;
+      deadlineCell.textContent = response.DueDate;
+    }
+  };
+  xhr.open("POST", "update_task.php");
+  xhr.send(formData);
+}
--- pages/components/task_review.php
+++ pages/components/task_review.php
@@ -1,28 +1,33 @@
 <!-- page for reviewing task results -->
-<form method="post" action=""> <!-- review_task.php is backend -->
-  <div class="form-group">
-    <label for="task-list">Search task</label>
-    <input type="text" class="form-control" id="task-search" name="task-search">
-  </div>
-  <div id="task-list"></div>
-  <div class="form-group">
-    <label for="comment">Comment</label>
-    <textarea class="form-control" id="comment" name="comment" rows="3"></textarea>
-  </div>
-  <div class="form-group">
-    <label for="status">Status</label>
-    <select class="form-control" id="status" name="status">
-      <option value="approved">Approved</option>
-      <option value="rejected">Rejected</option>
-      <option value="revise">Approved with revisions</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="new-deadline">New deadline (if rejected)</label>
-    <input type="date" class="form-control" id="new-deadline" name="new-deadline">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit Review</button>
-</form>
+<!-- Display tasks table -->
+<table>
+  <tr>
+    <th>Task</th>
+    <th>Status</th>
+    <th>Action</th>
+  </tr>
+  <tbody id="task-list">
+    <!-- Tasks will be displayed here -->
+  </tbody>
+</table>
+
+<!-- Template for task row -->
+<template id="task-template">
+  <tr>
+    <td class="task-name"></td>
+    <td class="task-status"></td>
+    <td>
+      <form class="task-form">
+        <input type="radio" name="status" value="approved">Approve
+        <input type="radio" name="status" value="rejected">Reject
+        <input type="radio" name="status" value="revisions">Approved with Revisions
+        <input type="text" name="deadline" placeholder="New Deadline">
+        <input type="button" value="Update" onclick="updateTask(this)">
+      </form>
+    </td>
+  </tr>
+</template>
+
 
 <script type="module" src="pages/components/task_review.js">
 
