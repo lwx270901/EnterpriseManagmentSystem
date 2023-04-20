c1d8ec3e618147ec8e690195505157c6c79e18e2
LWX270901 <letungocminh@gmail.com>
2023-04-20 23:58:17 +0700

    merge

--- pages/components/task_review.php
+++ pages/components/task_review.php
@@ -1,32 +1,5 @@
 <!-- page for reviewing task results -->
-<!-- Display tasks table -->
-<table>
-  <tr>
-    <th>Task</th>
-    <th>Status</th>
-    <th>Action</th>
-  </tr>
-  <tbody id="task-list">
-    <!-- Tasks will be displayed here -->
-  </tbody>
-</table>
 
-<!-- Template for task row -->
-<template id="task-template">
-  <tr>
-    <td class="task-name"></td>
-    <td class="task-status"></td>
-    <td>
-      <form class="task-form">
-        <input type="radio" name="status" value="approved">Approve
-        <input type="radio" name="status" value="rejected">Reject
-        <input type="radio" name="status" value="revisions">Approved with Revisions
-        <input type="text" name="deadline" placeholder="New Deadline">
-        <input type="button" value="Update" onclick="updateTask(this)">
-      </form>
-    </td>
-  </tr>
-</template>
 
 
 <script type="module" src="pages/components/task_review.js">
